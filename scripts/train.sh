2dd03acda5ffeecf0b0127c91eb5f499c9a79e56
ruipimentelfigueiredo <ruihortafigueiredo@gmail.com>
2017-04-19 14:09:43 +0100

    minor fixes

--- scripts/train.sh
+++ scripts/train.sh
@@ -7,7 +7,7 @@
 #    exit
 #fi
 
-DIR=$PWD; # active_semantic_mapping
+DIR=$PWD; # MUST BE RUN FROM active_semantic_mapping ROOT !!!!!!!!!!!!!!!!!!!!!!
 
 BASE_DATASET_FOLDER='/run/user/1000/gvfs/smb-share:server=blackhole,share=users/ruifigueiredo'
 
@@ -17,3 +17,9 @@ python $DIR/python/modify_dataset.py $BASE_DATASET_FOLDER
 
 # 2. create lmdb data
 python $DIR/python/create_lmdb.py $BASE_DATASET_FOLDER $DIR
+
+# 3. compute mean image
+compute mean.binaryproto /home/rui/mot/lib/caffe/build/tools/compute_image_mean -backend=lmdb $DIR/Cylinders/dataset_v3/train_lmdb $DIR/Cylinders/dataset_v3/mean.binaryproto
+
+# 4. train the network with the lmdb - mean data
+/home/rui/mot/lib/caffe/build/tools/caffe train --solver=$DIR/squeezenet/solver.prototxt --weights $DIR/squeezenet/squeezenet_v1.1.caffemodel 2>&1 | tee $DIR/Cylinders/dataset_v3/model_1_train.log
