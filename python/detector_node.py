5f6914a1850dd2a3ac0abde204fc7ca756e72e65
Atabak Dehban <atabakd@gmail.com>
2017-11-29 23:43:43 +0000

    Finished Bounding Box Publisher

--- python/detector_node.py
+++ python/detector_node.py
@@ -7,37 +7,47 @@ Created on Wed Nov 29 03:01:02 2017
 """
 from __future__ import print_function, absolute_import, division
 import rospy
-from std_msgs.msg import String
+from std_msgs.msg import String, Header
 import cv2
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
-from active_semantic_mapping.msg import BoundingBoxArray
+from active_semantic_mapping import msg
 
 import numpy as np
 import os
-import tensorflow as tf
 import sys
 import inspect
 
 def callback(data):
     bridge = CvBridge()
-    global my_detector
+    global my_detector, pub
 
     try:
       cv_image = bridge.imgmsg_to_cv2(data, "bgr8")
     except CvBridgeError as e:
       print(e)
 
-    # CALL LIBRARY METHOD (COMPUTE BOUNDINGBOXES)
-    #bounding_boxes=object.detect(cv_image)
     rgb_img = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
-#    cv2.imshow("Image window", cv_image)
     rgb_img_resize = cv2.resize(rgb_img, (300,300))
-    my_detector.overlay_shapes(rgb_img_resize)
-    cv2.imshow("Image window", rgb_img_resize)
-    cv2.waitKey(3)
+    boxes, scores, classes = my_detector.detect(rgb_img_resize, threshold=0.5)
+    BBA = list() # Bounding box array
+    BBA_msg = msg.BoundingBoxArray()
     
-    # TENSORFLOW CODE
+    for idx in range(len(classes)):
+      BB_msg = msg.BoundingBox()
+      box = boxes[idx] * 300
+      BB_msg.x1 = box[0].astype(np.uint32)
+      BB_msg.y1 = box[1].astype(np.uint32)
+      BB_msg.x2 = box[2].astype(np.uint32)
+      BB_msg.y2 = box[3].astype(np.uint32)
+      BB_msg.label = classes[idx].astype(np.uint32)
+      BB_msg.confidence = scores[idx].astype(np.float64)
+      BBA.append(BB_msg)
+      
+    BBA_msg.boxes = BBA
+    #rospy.loginfo("I heard %d", len(BBA))
+    BBA_msg.header = Header()
+    pub.publish(BBA_msg)
     
 def listener():
 
@@ -60,4 +70,9 @@ if __name__ == '__main__':
   sys.path.append(path_to_detector)
   from detector import Detector
   my_detector = Detector()
+  pub = rospy.Publisher('bounding_boxes', msg.BoundingBoxArray)
+#  BBA_msg.header = ....
+#  BBA_msg.boxes= [boxes]
+  
+  
   listener()
